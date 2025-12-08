a3372a5f9f549adc02f9ac96aa73f7b44516d910
Maet <>
2025-12-08 22:24:26 +0700

    no message

--- app/dashboard/thesis/[id]/page.tsx
+++ app/dashboard/thesis/[id]/page.tsx
@@ -883,8 +883,8 @@ export default function ThesisDetailPage() {
                           </Button>
                           
                           <Button variant="outline" className="w-full mt-2" asChild>
-                              <Link href="/dashboard/upload">
-                                  <UploadCloud className="mr-2 h-4 w-4" /> Upload New Version
+                              <Link href="/dashboard/upload#update">
+                                  <UploadCloud className="mr-2 h-4 w-4" /> Upload New Chapter
                               </Link>
                           </Button>
 
