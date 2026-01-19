42fa58b2d06f685bef3aaaeece77a58652e51b56
Maet <>
2026-01-19 18:23:10 +0700

    ไม่มีอะไรแค่ lauout code

--- app/api/query/thesisPub/route.ts
+++ app/api/query/thesisPub/route.ts
@@ -1,24 +1,26 @@
 // app/api/query/thesis/route.ts
-import { NextResponse } from 'next/server';
-import { connectDatabase } from '@/lib/databaseconnect';
-import { Thesis } from '@/lib/models/Thesis';
-import { User } from '@/lib/models/Users';
+import { NextResponse } from "next/server";
+import { connectDatabase } from "@/lib/databaseconnect";
+import { Thesis } from "@/lib/models/Thesis";
+import { User } from "@/lib/models/Users";
 
-export const dynamic = 'force-dynamic';
+export const dynamic = "force-dynamic";
 
 export async function GET() {
   try {
     await connectDatabase();
 
-    const Theses = await Thesis.find({ status: 'approved', isPublic:'true' })
-      .populate('author', 'firstName lastName')
-      .populate('advisor', 'firstName lastName')
+    const Theses = await Thesis.find({ status: "approved", isPublic: "true" })
+      .populate({ path: "author", select: "firstName lastName", model: User })
+      .populate({ path: "advisor", select: "firstName lastName", model: User })
       .sort({ createdAt: -1 });
 
     return NextResponse.json({ success: true, theses: Theses });
-
   } catch (error: any) {
-    console.error('Query Thesis Error:', error);
-    return NextResponse.json({ success: false, error: 'Server error' }, { status: 500 });
+    console.error("Query Thesis Error:", error);
+    return NextResponse.json(
+      { success: false, error: "Server error" },
+      { status: 500 }
+    );
   }
 }
